5bf78df74d9682c7a087734bb00c3e05f5c4c0fa
rakuenzi <rakuenzi@student.ethz.ch>
2021-03-21 21:06:37 +0100

    some clean up for the api

--- api/api_app.py
+++ api/api_app.py
@@ -6,30 +6,37 @@ Created on Sun Mar 21 14:41:22 2021
 @author: raffael
 """
 
+# import necessary packages
+from flask import Flask, render_template, request
+
+# import the prediction function
 from predict_sentiment import predict, dummy_tokenizer
 
-from flask import Flask, render_template, request
+# initiate Flask
 app = Flask(__name__)
 
+
+# start input form
 @app.route('/')
 def request_review():
-    # review = "Bad! The actors weren't motivated to do a good job."
     return render_template('enter_review.html')
 
 
-@app.route('/result',methods = ['POST', 'GET'])
+# show sentiment of review
+@app.route('/result', methods=['POST', 'GET'])
 def result():
-   if request.method == 'POST':
-      result = request.form
-      
-      review = result['review']
-      
-      sent = predict(review)
-      
-      return render_template("result.html",sentiment = sent)
-    
+    if request.method == 'POST':
 
+        # get review from input form
+        result = request.form
+        review = result['review']
+
+        # predict sentiment
+        prediction = predict(review)
+
+        return render_template("result.html", sentiment=prediction)
+
+
+# run the spplication
 if __name__ == '__main__':
-   app.run(debug = True)
-
-
+    app.run(debug=True)
