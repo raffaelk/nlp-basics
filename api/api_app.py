e775d4f03b8fa6eab911ad093095dc2c4ae5396e
rakuenzi <rakuenzi@student.ethz.ch>
2021-03-23 20:52:47 +0100

    Dockerfile

--- api/api_app.py
+++ api/api_app.py
@@ -7,14 +7,20 @@ Created on Sun Mar 21 14:41:22 2021
 """
 
 # import necessary packages
+import os
+import nltk
 from flask import Flask, render_template, request
 
 # import the prediction function
 from predict_sentiment import predict, dummy_tokenizer
 
+# load nltk data
+nltk.download('punkt')
+nltk.download('stopwords')
+nltk.download('wordnet')
+
 # initiate Flask
 app = Flask(__name__)
-
 
 # start input form
 @app.route('/')
@@ -37,6 +43,6 @@ def result():
         return render_template("result.html", sentiment=prediction)
 
 
-# run the spplication
+# run the application
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
