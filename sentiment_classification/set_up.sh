601212354839c9e77ce72ae6294f611e613feae3
rakuenzi <rakuenzi@student.ethz.ch>
2021-04-25 15:53:03 +0200

    bash script set up

---
+++ sentiment_classification/set_up.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# get the movie reviews
+curl -O http://www.cs.cornell.edu/people/pabo/movie-review-data/review_polarity.tar.gz
+mkdir review_polarity
+tar -xzf review_polarity.tar.gz -C ./review_polarity
+
+# get opinion lexicon
+curl -O -L http://www.cs.uic.edu/~liub/FBS/opinion-lexicon-English.rar
+unrar x opinion-lexicon-English.rar
+mv opinion-lexicon-English opinion-lexicon
+
+# delete archives
+rm review_polarity.tar.gz
+rm opinion-lexicon-English.rar
+
